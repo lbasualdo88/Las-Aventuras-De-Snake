b07fe58ca34636c3dff4172ab185bd5a87739c04
Leonardo Basualdo <lbasualdo88@gmail.com>
2025-05-25 20:36:04 -0300

    Agrego las siguientes clases: Personaje, Snake, Guardia, MetalGear, Posicion y Enemigo. Tabmbien en la clase juego, agrego el metodo iniciar() con un menu.

--- src/Juego.java
+++ src/Juego.java
@@ -1,20 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author leo-h
- */
+import java.util.Scanner;
+
 public class Juego {
 
-    /**
-     * Fecha de entrega 12 de junio del 2025
-     */
+    static Scanner leer = new Scanner(System.in);
+
     public static void main(String[] args) {
-        
+      
+
+        iniciar();
     }
-    
+
+    public static void iniciar() {
+        boolean salir = true;
+
+        do {
+            System.out.println("------------------------");
+            System.out.println("1- Iniciar Nueva Partida");
+            System.out.println("2- Continuar Partida");
+            System.out.println("3- Salir");
+            System.out.println("------------------------");
+            int opcion = leer.nextInt();
+            switch (opcion) {
+                case 1:
+                    //inicia nueva partida
+                    break;
+                case 2:
+                    //continua partida guardada
+                    break;
+                case 3:
+                    System.out.println("-------------------------------------");
+                    System.out.println("Si sales, pierdes la partida guardada");
+                    System.out.println("-------------------------------------");
+                    System.out.println("Quieres salir?:");
+                    System.out.println("1- SI");
+                    System.out.println("2- NO");
+                    int opcion2 = leer.nextInt();
+                    if (opcion2 == 1) {
+                        salir = false;
+                    }
+                    break;
+            }
+        } while (salir);
+
+    }
 }

aed8bebaad9fac5cd657a4026038ebb2b22b379f
Juan Manuel Mogliati <mogliatijm@gmail.com>
2025-06-05 00:59:54 -0300

    Refactored `Mapa` class to enhance Snake's movement and enemy placement; added new methods for objective establishment and guard detection.

--- src/Mapa.java
+++ src/Mapa.java
@@ -8,8 +8,7 @@ public class Mapa {
 
     /** Matriz bidimensional de objetos tipo Celda */
     private Celda[][] matriz;
-    private int filas;
-    private int columnas;
+    private int filas, columnas;
 
     // Constructor por defecto: Misión 1 (7x7)
     public Mapa() {
@@ -33,11 +32,6 @@ public class Mapa {
                 matriz[i][j] = new Celda(); // Cada celda empieza vacía
             }
         }
-
-        // se valida si la posición [filas-1][0] existe (para Snake)
-        if (filas > 0 && columnas > 0) {
-            matriz[filas - 1][0].setTipo('S'); // Snake en esquina inferior izquierda
-        }
     }
 
     public void mostrarMapa() {
@@ -49,26 +43,107 @@ public class Mapa {
         }
     }
 
-    public void ubicarEnemigos(int cantidad) {
+    public void ubicarEnemigos(int cantidad, Posicion snakePos) {
         Random rand = new Random();
 
-        // Posición inicial de Snake
-        int snakeFila = filas - 1;
-        int snakeColumna = 0;
+        int snakeFila = snakePos.getY();
+        int snakeColumna = snakePos.getX();
 
         int colocados = 0;
         while (colocados < cantidad) {
             int fila = rand.nextInt(filas);
             int columna = rand.nextInt(columnas);
-
             int distancia = Math.abs(fila - snakeFila) + Math.abs(columna - snakeColumna);
-            boolean esZonaSegura = (distancia >= 2);
 
+            boolean esZonaSegura = (distancia >= 2);
             if (!matriz[fila][columna].isOcupada() && esZonaSegura) {
                 matriz[fila][columna].setTipo('G');
                 colocados++;
             }
         }
+    }
+
+    public Posicion establecerObjetivo(char tipo) {
+        Random rand = new Random();
+
+        while (true) {
+            int fila = rand.nextInt(filas);
+            int columna = rand.nextInt(columnas);
+
+            if (!matriz[fila][columna].isOcupada()) {
+                matriz[fila][columna].setTipo(tipo);
+                return new Posicion(columna, fila);
+            }
+        }
+    }
+
+    public char moverSnake(Snake snake, String direccion) {
+        Posicion actual = snake.getPosicion();
+        int xActual = actual.getX();
+        int yActual = actual.getY();
+
+        Posicion destino = new Posicion(xActual, yActual);
+
+        switch (direccion) {
+            case "w" -> destino.moverArriba();
+            case "s" -> destino.moverAbajo();
+            case "a" -> destino.moverIzquierda();
+            case "d" -> destino.moverDerecha();
+            default -> {
+                System.out.println("Se debe usar una de las siguientes teclas: w/a/s/d");
+                return '.'; // no se mueve, devuelve '.' como tipo “sin cambio”
+            }
+        }
+        int xDestino = destino.getX();
+        int yDestino = destino.getY();
+        if (xDestino < 0 || xDestino >= columnas || yDestino < 0 || yDestino >= filas) {
+            System.out.println("Movimiento fuera del mapa. Quedas en la misma casilla.");
+            return '.';
+        }
+
+        char tipoAntiguo = matriz[yDestino][xDestino].getTipo();
+
+        matriz[yActual][xActual].setOcupada(false);
+
+        snake.getPosicion().setX(xDestino);
+        snake.getPosicion().setY(yDestino);
+
+        matriz[yDestino][xDestino].setTipo('S');
+
+        return tipoAntiguo;
+    }
+
+    public boolean hayGuardiaCerca(Posicion posicion) {
+        int x = posicion.getX();
+        int y = posicion.getY();
+
+        // verificar celda arriba
+        if (y - 1 >= 0) {
+            if (matriz[y - 1][x].getTipo() == 'G') {
+                return true;
+            }
+        }
+
+        // verificar celda abajo
+        if (y + 1 < filas) {
+            if (matriz[y + 1][x].getTipo() == 'G') {
+                return true;
+            }
+        }
+
+        // verificar celda izquierda
+        if (x - 1 >= 0) {
+            if (matriz[y][x - 1].getTipo() == 'G') {
+                return true;
+            }
+        }
+
+        // verificar celda derecha
+        if (x + 1 < columnas) {
+            return matriz[y][x + 1].getTipo() == 'G';
+        }
+
+        return false;
     }
 
     // Getter opcional x si necesitamos  acceder al mapa desde otras clases
